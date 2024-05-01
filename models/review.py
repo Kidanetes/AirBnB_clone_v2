5541f5c58956a3bf87fbc6cbf25e42a289e1dec6
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-01 13:06:40 +0300

    adding nullable=False to text attribute

--- models/review.py
+++ models/review.py
@@ -9,7 +9,7 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024))
+        text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     else:

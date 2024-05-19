9eff4cb2b206650a74ae3f41a9b8114d903a34cd
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-19 19:14:39 +0300

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,6 +2,7 @@
 """create a compressed file of the web_static directory and
 deploy them to the servers"""
 
+
 from 1-pack_web_static import do_pack
 from 2-do_deploy_web_static import do_deploy
 from fabric.api import *
@@ -11,8 +12,10 @@ env.hosts = ['52.86.189.212', '52.201.146.212']
 
 
 def deploy():
-    """deploy the webstatic directory to the servers"""
+    """
+    deploy the webstatic directory to the servers
+    """
     x = do_pack()
     if x is None:
         return False
-    return  do_deploy(x)
+    return do_deploy(x)

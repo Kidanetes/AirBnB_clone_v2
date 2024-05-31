00e2f3539449c863169f1fbd6361ed180243e432
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-31 21:28:38 +0300

    route if n is only an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""this module contains implementation
+of flask framework"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """this function return hello HBNB!
+    routes /"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """this function return HBNB when we
+    route /hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """this function returns c + <text> when we
+    route /c/<text>"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>")
+def python_is_cool(text="is_cool"):
+    """this function returns python + <text>
+    default value of text is cool"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_only(n):
+    """this function only routes only if
+    n is integer"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

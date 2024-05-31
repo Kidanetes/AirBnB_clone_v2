f8a35650a8b820fe1c98439304e19666456dc6f4
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-05-31 21:22:25 +0300

    variable with default varibale

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""this module contains implementation
+of flask framework"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """this function return hello HBNB!
+    routes /"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """this function return HBNB when we
+    route /hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """this function returns c + <text> when we
+    route /c/<text>"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>")
+def python_is_cool(text="is_cool"):
+    """this function returns python + <text>
+    default value of text is cool"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

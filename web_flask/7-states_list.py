6a14314589a9d254c841f9fff388f8d9e92f2927
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-06-02 04:43:57 +0300

    state list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from models import storage
+from flask import render_template
+from models.state import State
+
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_request(self):
+    """close the session after each request"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """list every state in the storage"""
+    dict1 = storage.all(State).values()
+    
+    return render_template('7-states_list.html', dict1=dict1)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    #close_request()

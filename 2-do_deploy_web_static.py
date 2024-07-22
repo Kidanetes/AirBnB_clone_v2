19e02369cfae132c6f373980bd2e64d96d1de649
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-07-22 15:58:34 +0300

    do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ webstatic directory"""
 from fabric.api import *
 from os.path import exists
 
-env.hosts = ['54.152.218.147', '54.87.157.200']
+env.hosts = ['3.85.136.6', '18.209.152.171']
 
 
 def do_deploy(archive_path):
@@ -18,15 +18,15 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         archive_name = archive_path.split("/")[-1].split(".")[0]
         unarchive_name = archive_path.split("/")[-1]
-        run(f"mkdir -p /data/web_static/releases/{archive_name}")
-        run(f"tar -xzf  /tmp/{unarchive_name} -C \
+        run(f"sudo mkdir -p /data/web_static/releases/{archive_name}")
+        run(f"sudo tar -xzf  /tmp/{unarchive_name} -C \
               /data/web_static/releases/{archive_name}/")
-        run(f"rm /tmp/{unarchive_name}")
-        run(f"mv  /data/web_static/releases/{archive_name}/web_static/* \
+        run(f"sudo rm /tmp/{unarchive_name}")
+        run(f"sudo mv  /data/web_static/releases/{archive_name}/web_static/* \
              /data/web_static/releases/{archive_name}")
-        run(f"rm -rf /data/web_static/releases/{archive_name}/web_static")
-        run("rm -rf /data/web_static/current")
-        run(f"ln -s /data/web_static/releases/{archive_name}/ \
+        run(f"sudo rm -rf /data/web_static/releases/{archive_name}/web_static")
+        run("sudo rm -rf /data/web_static/current")
+        run(f"sudo ln -s /data/web_static/releases/{archive_name}/ \
              /data/web_static/current")
         return True
     except Exception:

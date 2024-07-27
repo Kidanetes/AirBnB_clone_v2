0501ff97751298218d0674b0dc17b066df182dd8
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-07-27 15:31:10 +0300

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+flask minmimal application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """returns the route /"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return the route /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    """return c + text"""
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is_cool")
+    """return python + text"""
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

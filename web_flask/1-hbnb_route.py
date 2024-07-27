64a0af9b6be0719b983c7084b3a2b49048a6ebb7
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-07-27 12:27:52 +0300

    pycodestyle fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,12 @@ def root():
     """returns the route /"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """return the route /hbnb"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')

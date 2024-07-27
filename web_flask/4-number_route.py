516b23f9cc772f5261255d7fc9cc5044157aef0d
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-07-27 15:46:55 +0300

    n is number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+flask minmimal application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """returns the route /"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return the route /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    """return c + text"""
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is_cool"):
+    """return python + text"""
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_number(n):
+    """display n if it is integer"""
+    if type(n) is int:
+        return str(n) + " is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

572dda02480a74dd560c04c36e868defce24b990
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-07-28 16:34:01 +0300

    web_flask

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,45 +1,35 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""this module contains two functions
+which will return list of states in the
+storage"""
+
+
+from flask import Flask
 from models import storage
+from flask import render_template
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def close_request(self):
+    """close the session after each request"""
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    return render_template('100-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places)
+@app.route("/hbnb", strict_slashes=False)
+def state_list():
+    """list state and cities associated with them
+    and amenities"""
+    l1 = storage.all(State).values()
+    l2 = storage.all(Amenity).values()
+    l3 = storage.all(Place).values()
+    return render_template('100-hbnb.html', l1=l1, l2=l2, l3=l3)
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
